66d7f9ff9615440076cf5e1d7c4fa0f17a99db02
Daniel Nicolai <dalanicolai@gmail.com>
2021-06-13 11:11:19 +0200

    Add keyboard annotation command

--- lisp/pdf-annot.el
+++ lisp/pdf-annot.el
@@ -137,6 +137,12 @@ to \"Joe\"."
             (cons :tag "Squiggly Annotations" (const squiggly) ,markup-properties)
             (cons :tag "Strike-out Annotations" (const strike-out) ,markup-properties))))
 
+(defcustom pdf-annot-keyboard-annot-default-type "highlight"
+  "Default annotation type for keyboard annotation"
+  :type '(string)
+  :options '("highlight" "squiggly" "strike-out" "underline")
+  :group 'pdf-annot)
+
 (defcustom pdf-annot-print-annotation-functions
   '(pdf-annot-print-annotation-latex-maybe)
   "A alist of functions for printing annotations, e.g. for the tooltip.
@@ -1201,6 +1207,76 @@ See also `pdf-annot-add-markup-annotation'."
 See also `pdf-annot-add-markup-annotation'."
   (interactive (list (pdf-view-active-region t)))
   (pdf-annot-add-markup-annotation list-of-edges 'highlight color property-alist))
+
+(defun pdf-annot-keyboard-annot-format-collection (search-results)
+  "Transform SEARCH-RESULTS into useful collection.
+The collection is given to completing-read in the
+`pdf-annot-keyboard-annotate' function."
+  (mapcar (lambda (x)
+            (let ((y (cdr x)))
+              (cons (cdar y)
+                    (cdr y))))
+          search-results))
+
+(defun pdf-annot-keyboard-annotate (&optional arg)
+  "Create markup annotation using the keyboard.
+Prompts for start pattern and end pattern for the text region to
+annotate (pattern can be only beginning/ending part of a word or
+multiple words). If any pattern returns multiple matches then
+additionally prompt for selecting the correct region(s). Creates
+type of `pdf-annot-keyboard-annot-default-type' by default. When
+prefixed with universal argument \\[universal-argument], the
+command additionally prompts for selecting an annotation type.
+
+Unfortunately, in some documents the edges (i.e. size of the
+region) are not translated correctly."
+  (interactive "P")
+  (pdf-tools-assert-pdf-buffer)
+  (let* ((from (pdf-annot-keyboard-annot-format-collection
+                (pdf-info-search-string (read-string "From: ")
+                                        (pdf-view-current-page))))
+         (to (let ((patt (read-string "To: ")))
+               (unless (string= patt "")
+                 (pdf-annot-keyboard-annot-format-collection
+                  (pdf-info-search-string patt
+                                          (pdf-view-current-page))))))
+         (start-coords (if (= (length from) 1)
+                           (cadr (cadar from))
+                         (cadar (alist-get
+                                 (completing-read "Select correct START context: " from)
+                                 from nil nil 'equal))))
+         ;; list only to's that come after the selected start region
+         (tos-after-start (delete nil
+                                  (mapcar (lambda (x)
+                                            (let ((coords (car (cdadr x))))
+                                              (if (> (nth 1 coords) (nth 3 start-coords))
+                                                  x
+                                                (when (and (= (nth 1 coords) (nth 1 start-coords))
+                                                           (> (nth 0 coords) (nth 2 start-coords)))
+                                                  x))))
+                                          to)))
+         (end-coords (when to
+                       (if (= (length to) 1)
+                           (cadr (cadar to))
+                         (cadar (alist-get
+                                 (completing-read "Select correct END context: " tos-after-start)
+                                 tos-after-start nil nil 'equal)))))
+         (edges (if to
+                    (append (cl-subseq start-coords 0 2) (cl-subseq end-coords 2 4))
+                  start-coords)))
+    (pcase (if arg
+               (read-answer "Create annotation of markup type? "
+                '(("highlight"  ?h "perform the action")
+                  ("squiggly"   ?s "skip to the next")
+                  ("strike-out" ?o "accept all remaining without more questions")
+                  ("underline"  ?u "accept all remaining without more questions")
+                  ("help" ?h "show help")
+                  ("quit" ?q "exit")))
+             pdf-annot-keyboard-annot-default-type)
+      ("highlight"  (pdf-annot-add-highlight-markup-annotation edges))
+      ("squiggly"   (pdf-annot-add-squiggly-markup-annotation edges))
+      ("strike-out" (pdf-annot-add-strikeout-markup-annotation edges))
+      ("underline"  (pdf-annot-add-underline-markup-annotation edges)))))
 
 (defun pdf-annot-read-color (&optional prompt)
   "Read and return a color using PROMPT.
