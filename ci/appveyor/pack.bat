5f209643e98fee516166e813b0c3e8f5670515e9
Jeremy Meng <jeremy.ymeng@gmail.com>
2021-09-24 17:00:42 +0000

    Update Appveyor configs
    - Use VS 2019 image
    - Combine pacman install commands
    - For some reason, OpenSSL dlls were not copied as dependencies. Update to copy
    explicitly. Also remove previously workaround.

--- ci/appveyor/pack.bat
+++ ci/appveyor/pack.bat
@@ -13,9 +13,9 @@ IF %COMPILER%==msys2 (
   REM Copy epdfinfo.exe and all dependencies
   bash -lc "pushd /c/projects/pdf-tools; ldd server/epdfinfo.exe | grep mingw | cut -d' ' -f 3 | xargs -I {} cp {} ./pack/; cp server/epdfinfo.exe ./pack/"
 
-  REM Copy dlls to pack
-  cp %TMPDIR%\ssleay32.dll %PACKDIR%\
-  cp %TMPDIR%\libeay32.dll %PACKDIR%\
+  REM Copy openssl dlls to pack
+  copy C:\%MSYS2_DIR%\%MSYSTEM%\bin\libcrypto*.dll %PACKDIR%\
+  copy C:\%MSYS2_DIR%\%MSYSTEM%\bin\libssl*.dll %PACKDIR%\
 
   REM Package epdfinfo.exe and all dependencies
   7z a epdfinfo.zip %PACKDIR%\*.*
