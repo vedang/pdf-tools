e8efff32f676a0b59f3bba3ef92c52f16b639c3e
Jeremy Meng <jeremy.ymeng@gmail.com>
2021-09-24 17:00:42 +0000

    Update Appveyor configs
    - Use VS 2019 image
    - Combine pacman install commands
    - For some reason, OpenSSL dlls were not copied as dependencies. Update to copy
    explicitly. Also remove previously workaround.

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -9,30 +9,13 @@ echo MSYS2 directory: %MSYS2_DIR%
 echo MSYS2 system: %MSYSTEM%
 echo Bits: %BIT%
 
-REM Create a writeable TMPDIR
-mkdir %APPVEYOR_BUILD_FOLDER%\tmp
-set TMPDIR=%APPVEYOR_BUILD_FOLDER%\tmp
-
 IF %COMPILER%==msys2 (
   @echo on
   SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;C:\%MSYS2_DIR%\home\appveyor\.cask\bin;%PATH%"
 
-  bash -lc "pacman -S --needed --noconfirm git"
-
   REM dependencies
-  bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-zlib mingw-w64-x86_64-libpng mingw-w64-x86_64-poppler mingw-w64-x86_64-imagemagick openssl mingw-w64-x86_64-openssl mingw-w64-x86_64-python2"
-
-  REM Set up emacs
-  bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-emacs"
+  bash -lc "pacman -S --needed --noconfirm git mingw-w64-x86_64-zlib mingw-w64-x86_64-libpng mingw-w64-x86_64-poppler mingw-w64-x86_64-imagemagick openssl mingw-w64-x86_64-openssl mingw-w64-x86_64-python2 mingw-w64-x86_64-emacs"
 
   REM Set up Cask
   bash -lc "git clone https://github.com/cask/cask ~/.cask"
-
-  REM Download ssleay32 and libeay32 dlls
-  bash -lc "pushd /c/projects/pdf-tools; curl -fsSL https://indy.fulgan.com/SSL/openssl-1.0.2u-x64_86-win64.zip -o ./tmp/openssl.zip"
-
-  REM Extract *eay32 dlls to TMP
-  7z x -o%TMPDIR%\ %TMPDIR%\openssl.zip
-  bash -lc "pushd /c/projects/pdf-tools; echo Indy Fulgan download complete; pwd; ls -la ./tmp/"
-
 )
