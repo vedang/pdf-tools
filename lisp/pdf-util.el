8f6db904a75f85082854d734f08b90ecbbc86c54
Roshan Shariff <roshan.shariff@gmail.com>
2021-10-01 16:18:41 -0600

    Sanitize buffer name for use as temp file prefix
    Prefixes for temporary files are derived from either the file name
    associated with the pdf-view buffer, or the buffer name if it is not
    visiting a file.  If the buffer name contains '/' characters, then it is
    not a valid file name.
    
    Clean up non-alphanumeric characters in the buffer name before using
    it as a temp file prefix.

--- lisp/pdf-util.el
+++ lisp/pdf-util.el
@@ -558,12 +558,7 @@ killed."
       (let ((temporary-file-directory
              pdf-util--base-directory))
         (setq pdf-util--dedicated-directory
-              (make-temp-file (convert-standard-filename
-			       (concat (if buffer-file-name
-					   (file-name-nondirectory
-					    buffer-file-name)
-					 (buffer-name))
-				       "-"))
+              (make-temp-file (convert-standard-filename (pdf-util-temp-prefix))
                               t))
         (add-hook 'kill-buffer-hook 'pdf-util-delete-dedicated-directory
                   nil t)))
@@ -577,13 +572,21 @@ killed."
   "Expand filename against current buffer's dedicated directory."
   (expand-file-name name (pdf-util-dedicated-directory)))
 
-(defun pdf-util-make-temp-file (prefix &optional dir-flag suffix)
+(defun pdf-util-temp-prefix ()
+  "Create a temp-file prefix for the current buffer"
+  (concat (if buffer-file-name
+              (file-name-nondirectory buffer-file-name)
+            (replace-regexp-in-string "[^[:alnum:]]+" "-" (buffer-name)))
+          "-"))
+
+(defun pdf-util-make-temp-file (&optional prefix dir-flag suffix)
   "Create a temporary file in current buffer's dedicated directory.
 
 See `make-temp-file' for the arguments."
-  (let ((temporary-file-directory
-         (pdf-util-dedicated-directory)))
-    (make-temp-file (convert-standard-filename prefix) dir-flag suffix)))
+  (let ((temporary-file-directory (pdf-util-dedicated-directory)))
+    (make-temp-file (convert-standard-filename
+                     (or prefix (pdf-util-temp-prefix)))
+                    dir-flag suffix)))
 
 
 ;; * ================================================================== *
